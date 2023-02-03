b82c99aeaaa7ea539bd4b1c482d76f51b2c6f828
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-02-03 15:58:45 -0800

    add User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },    
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual to retrieve length of user's friends array
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
+
+// Initialize User model
+const User = model('user', userSchema);
+
+module.exports = User;
